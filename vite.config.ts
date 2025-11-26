5a306b42a9598da9b8f741b107762a03b15a3b28
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-11-26 18:38:56 -0300

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: { host: '::' },
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+      overlay: false,
+      biome: {
+        command: 'check',
+      },
+    }),
+  ],
+});
